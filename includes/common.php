a9c61816a66ced6f6728c6d08b46bac13faa1460
Ashish Mishra <61447731+ashishm306@users.noreply.github.com>
2020-07-29 12:40:39 +0530

    Update common.php

--- includes/common.php
+++ includes/common.php
@@ -1,4 +1,6 @@
 <?php
-$con= mysqli_connect("localhost", "root", "", "lifestylestore") or die(mysqli_error($con));
-session_start();
+$con= mysqli_connect("localhost", "id14484423_ashishm306", "<BL\B>5l6WnZJZWU", "id14484423_lifestylestore") or die(mysqli_error($con));
+ if(!isset($_SESSION)){
+      session_start();
+    }
 ?>
